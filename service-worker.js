4b6f269d0a96c7bfb3d4622f4be5ac72c11f8e8d
tabuchi20 <takuya.tabuchi@tsuwasangyou.com>
2025-12-18 18:21:58 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = "qr-check-v2";
+const CACHE_NAME = "qr-check-v4";
 
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
-  "./tenyuryoku.html",
-  "./scan.html",
+  "./tenyuryoku.html",   // ← 手入力検品
+  "./scan.html",         // ← 海外ラベル
 
   "./manifest.webmanifest",
   "./service-worker.js",
 
   "./js/jsQR.js",
+  "./js/zxing.min.js",
 
   "./sounds/yomitori.mp3",
   "./sounds/seikou.mp3",
@@ -18,3 +19,15 @@ const URLS_TO_CACHE = [
   "./icon/favicon.png",
   "./icon/icon-180.png"
 ];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
