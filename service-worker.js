4a2a6084ab10a164c7c6b33821e191cde05e60f5
tabuchi20 <takuya.tabuchi@tsuwasangyou.com>
2025-12-22 14:44:50 +0900

    キャッシュ削除

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "qr-check-v8";
+const CACHE_NAME = "qr-check-v9";
 
 const URLS_TO_CACHE = [
   "./",
@@ -20,14 +20,29 @@ const URLS_TO_CACHE = [
   "./icon/icon-180.png"
 ];
 
+// インストール時：キャッシュ
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
   );
 });
 
+// 古いキャッシュ削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+// 通信時：キャッシュ優先
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
